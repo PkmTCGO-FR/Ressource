ebdbeefcf8c7b99ef0425c2c4676590309300629
Sayen-Raigin <36884403+Sayen-Raigin@users.noreply.github.com>
2018-08-13 21:53:14 +0200

    tesy

---
+++ BOT/BotTcgo - 1.0/Objets/Membres/ClassMembreDiscord.js
@@ -0,0 +1,420 @@
+exports.user1 ={
+"id":process.env.id1,
+"idTcgo":"Sayen-Alpha",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user2 ={
+"id":process.env.id2,
+"idTcgo":"poképoké666",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user3 ={
+"id":process.env.id3,
+"idTcgo":"Ombravageur",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user4 ={
+"id":process.env.id4,
+"idTcgo":"HardLead7",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user5 ={
+"id":process.env.id5,
+"idTcgo":"EXTERMINATOR23",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user6 ={
+"id":process.env.id6,
+"idTcgo":"Lunette97423",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user7 ={
+"id":process.env.id7,
+"idTcgo":"Mega-Drattak",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user8 ={
+"id":process.env.id8,
+"idTcgo":"Newpierrick974",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user9 ={
+"id":process.env.id9,
+"idTcgo":"LouDorée",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user10 ={
+"id":process.env.id10,
+"idTcgo":"Meuronne",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user11 ={
+"id":process.env.id11,
+"idTcgo":"baloulo1",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+exports.user12 ={
+"id":process.env.id12,
+"idTcgo":"DarkLucario76",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+
+exports.user13 ={
+"id":process.env.id13,
+"idTcgo":"lekev94",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+
+exports.user14 ={
+"id":process.env.id14,
+"idTcgo":"shonight",
+"NmbTournoiFait":"",
+"NmbTournoiWin":""
+}
+
+exports.user15 ={
+"id":process.env.id15,
+"idTcgo":"RyumiSenkai",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user16 ={
+"id":process.env.id16,
+"idTcgo":"herazalila",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user17 ={
+"id":process.env.id17,
+"idTcgo":"yannick_9",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user18 ={
+"id":process.env.id18,
+"idTcgo":"DeoxysFan14",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user19 ={
+"id":process.env.id19,
+"idTcgo":"rene360",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user20 ={
+"id":process.env.id20,
+"idTcgo":"shinrei14",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user21 ={
+"id":process.env.id21,
+"idTcgo":"XenHedin",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user22 ={
+"id":process.env.id22,
+"idTcgo":"Megason",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user23 ={
+"id":process.env.id23,
+"idTcgo":"Alme61",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user24 ={
+"id":process.env.id24,
+"idTcgo":"azyox36",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user25 ={
+"id":process.env.id25,
+"idTcgo":"darradada",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user26 ={
+"id":process.env.id26,
+"idTcgo":"Pokemon038",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user27 ={
+"id":process.env.id27,
+"idTcgo":"GernoxySenpai",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user28 ={
+"id":process.env.id28,
+"idTcgo":"ordstir",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user29 ={
+"id":process.env.id29,
+"idTcgo":"killer42330",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user30 ={
+"id":process.env.id30,
+"idTcgo":"Leoniblood",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user31 ={
+"id":process.env.id31,
+"idTcgo":"Mexic1",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user32 ={
+"id":process.env.id32,
+"idTcgo":"Shabladow",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user33 ={
+"id":process.env.id33,
+"idTcgo":"MrTiTi3000",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user34 ={
+"id":process.env.id34,
+"idTcgo":"MisterJeremX",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user35 ={
+"id":process.env.id35,
+"idTcgo":"Mrquentinous",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+
+exports.user36 ={
+"id":process.env.id36,
+"idTcgo":"shilobi",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user37 ={
+"id":process.env.id37,
+"idTcgo":"Keith_90",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user38 ={
+"id":process.env.id38,
+"idTcgo":"nonolesse",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user39 ={
+"id":process.env.id39,
+"idTcgo":"SephJul",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user40 ={
+"id":process.env.id40,
+"idTcgo":"flogoo36",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user41 ={
+"id":process.env.id41,
+"idTcgo":"Ulmin",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user42 ={
+"id":process.env.id42,
+"idTcgo":"DarkNinja623",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user43 ={
+"id":process.env.id43,
+"idTcgo":"Nicolas86000",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user44 ={
+"id":process.env.id44,
+"idTcgo":"Lalbi93dz",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user45 ={
+"id":process.env.id45,
+"idTcgo":"veninox",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user46 ={
+"id":process.env.id46,
+"idTcgo":"MegAOpTic44",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user47 ={
+"id":process.env.id47,
+"idTcgo":"Greediesman",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user48 ={
+"id":process.env.id48,
+"idTcgo":"keelydey",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user49 ={
+"id":process.env.id49,
+"idTcgo":"omedidou",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user50 ={
+"id":process.env.id50,
+"idTcgo":"Lussarc",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user51 ={
+"id":process.env.id51,
+"idTcgo":"Kebab_Dancer",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user52 ={
+"id":process.env.id52,
+"idTcgo":"OnlyCute",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user53 ={
+"id":process.env.id53,
+"idTcgo":"Aihry",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user54 ={
+"id":process.env.id54,
+"idTcgo":"Anarshyz",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user55 ={
+"id":process.env.id55,
+"idTcgo":"Kiilou94",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user56 ={
+"id":process.env.id56,
+"idTcgo":"HBastien",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user57 ={
+"id":process.env.id57,
+"idTcgo":"DarkMatrix92",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user58 ={
+"id":process.env.id58,
+"idTcgo":"Kow6901",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user59 ={
+"id":process.env.id59,
+"idTcgo":"Astras",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user60 ={
+"id":process.env.id60,
+"idTcgo":"Acouphene",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user61 ={
+"id":process.env.id61,
+"idTcgo":"Marc851412",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user62 ={
+"id":process.env.id62,
+"idTcgo":"Ckinnie",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user63 ={
+"id":process.env.id63,
+"idTcgo":"Jiotos",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user64 ={
+"id":process.env.id64,
+"idTcgo":"Sneezze",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user65 ={
+"id":process.env.id65,
+"idTcgo":"Lubyllule",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
+exports.user66 ={
+"id":process.env.id66,
+"idTcgo":"Rixel9",
+"NmbTournoiFait":"",
+"NmbTournoiWin":"",
+}
