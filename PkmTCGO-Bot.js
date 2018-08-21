32d2d4d3cfbc47c52d2c555d051c3262407a7e53
Sayen-Raigin <36884403+Sayen-Raigin@users.noreply.github.com>
2018-08-21 19:33:34 +0200

    Update Ressource folder

--- PkmTCGO-Bot.js
+++ PkmTCGO-Bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
-const PathFoldersObject = "./Objets/Membres";
+const PathFoldersObject = "Bot-Ressource";
 var Prefix='!';
 
 bot.login(process.env.PremierPointTcgNon);
