0f2ec307de213e9817e51d3d19c84fcba5e323e1
Sayen-Raigin <36884403+Sayen-Raigin@users.noreply.github.com>
2018-08-22 20:44:54 +0200

    t

--- Bot/PkmTCGO-Bot.js
+++ Bot/PkmTCGO-Bot.js
@@ -104,7 +104,7 @@ bot.on('message', message => {
         
         if(splitMessage.length === 3){
             
-            var Annees=fs.readdirSync(PathDeckListe+"/Standard/", (err, files) => {files.length});
+            var Annees=fs.readdirSync(PathDeckListe+"/Standard/", (err, files) => {files.length}),
                 Format=fs.readdirSync(PathDeckListe+'/', (err, files) => {files.length}),
                 chemin;
             message.channel.sendMessage(Annees)
