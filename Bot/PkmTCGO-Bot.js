605163e45750d8f858fab7fe8312919ffd776eb7
Sayen-Raigin <36884403+Sayen-Raigin@users.noreply.github.com>
2018-08-22 20:34:21 +0200

    t

--- Bot/PkmTCGO-Bot.js
+++ Bot/PkmTCGO-Bot.js
@@ -107,7 +107,7 @@ bot.on('message', message => {
             //var Annees=fs.readdirSync(PathDeckListe+"/Standard/", (err, files) => {files.length});
                 //Format=fs.readdirSync(PathDeckListe+'/', (err, files) => {files.length}),
                 //chemin;
-            message.channel.sendMessage(fs.readFileSync("/app/Bot/test.txt", "UTF-8"))
+            //message.channel.sendMessage(fs.readFileSync("/app/Bot/test.txt", "UTF-8"))
             //console.log(fs.constants)
             
             
